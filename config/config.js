4008c9b1d89733dba0b1e1c116bf684a3a9c8c9d
Lewinskie <wangaralewis@gmail.com>
2024-08-17 20:51:43 +0300

    few tweaks

--- config/config.js
+++ config/config.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
-
 module.exports = {
   development: {
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
     dialect: "sqlite",
-    storage: process.env.DATABASE_URL || "./database/development.sqlite3",
-  },
-  production: {
-    dialect: "sqlite",
-    storage: process.env.DATABASE_URL || "./database/production.sqlite3",
+    storage: process.env.DATABASE_URL_DEV, //Pointing to the dev database
   },
   staging: {
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
     dialect: "sqlite",
-    storage: process.env.DATABASE_URL || "./database/staging.sqlite3",
+    storage: process.env.DATABASE_URL_STAGING, //Pointing to the staging database
+  },
+  production: {
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    dialect: "sqlite",
+    storage: process.env.DATABASE_URL_PROD, //Pointing to the production database
   },
 };
